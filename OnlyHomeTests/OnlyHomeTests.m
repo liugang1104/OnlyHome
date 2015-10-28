bbe055e81c94a3527e010040f808d94ee4e78bc2
plpq <1500010070@qq.com>
2015-10-28 14:11:27 +0800

    Initial Commit

---
+++ OnlyHomeTests/OnlyHomeTests.m
@@ -0,0 +1,39 @@
+//
+//  OnlyHomeTests.m
+//  OnlyHomeTests
+//
+//  Created by huxianwen on 15/10/28.
+//  Copyright © 2015年 huxianwen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OnlyHomeTests : XCTestCase
+
+@end
+
+@implementation OnlyHomeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
